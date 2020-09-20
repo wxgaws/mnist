99647a16a0d32f06911debd1eafa7947d7f78b38
wxgaws <wxgaws@163.com>
2020-09-20 12:09:16 +0800

    tf1.14中，keras.fit使用dataset时，不能设置batch_size

--- dnn.py
+++ dnn.py
@@ -4,14 +4,12 @@ import os
 #
 import to_dataset
 
+EPOCHS=2
+
 path_train = os.path.join(to_dataset.dir_main, "data/dataset/mnist.train.tfrecord")
 path_test = os.path.join(to_dataset.dir_main, "data/dataset/mnist.test.tfrecord")
 dataset_train = to_dataset.read_dataset(path_train)
 dataset_test = to_dataset.read_dataset(path_test)
-
-print(to_dataset.dir_main)
-print(path_train)
-print(os.path.join("D:/work/program/python/tf/mnist", "./data/dataset/mnist.train.tfrecord"))
 
 model = Sequential()
 model.add(Dense(100, activation='relu', input_shape=(to_dataset.image_size,)))
@@ -20,5 +18,5 @@ model.add(Dense(10, activation='softmax'))
 model.summary()
 
 model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
-model.fit(dataset_train, batch_size=to_dataset.batch_size, epochs=2, steps_per_epoch=to_dataset.steps_per_epoch_train)
-# model.fit(dataset_test, batch_size=to_dataset.batch_size, epochs=2, steps_per_epoch=to_dataset.steps_per_epoch_test)
+model.fit(dataset_train, epochs=EPOCHS, steps_per_epoch=to_dataset.steps_per_epoch_train)
+# model.fit(dataset_test, epochs=EPOCHS, steps_per_epoch=to_dataset.steps_per_epoch_test)
